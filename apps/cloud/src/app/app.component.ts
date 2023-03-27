138ac772f9dd5197995feebf35cf98fda49722ba
Ian Westerfield <ianwesterfield@users.noreply.github.com>
2023-03-27 01:30:04 +0000

    Added initial project structure

---
+++ apps/cloud/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'westerfield-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'cloud';
+}
