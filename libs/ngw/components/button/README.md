e813639a238e11d99022e8d510a2c654158e8c1a
Ian Westerfield <ian.westerfield@enquiren.com>
2023-06-29 17:28:03 -0600

    upgrading to Angular 16

--- libs/ngw/components/button/README.md
+++
@@ -1,3 +0,0 @@
-# @westerfield/ngw/components/button
-
-Secondary entry point of `@westerfield/ngw`. It can be used by importing from `@westerfield/ngw/components/button`.
--- libs/ngw/components/button/src/button/button.directive.ts
+++
@@ -1,8 +0,0 @@
-import { Directive, HostBinding } from '@angular/core';
-
-@Directive({
-  selector: '[ngw-button]'
-})
-export class ButtonDirective {
-  @HostBinding('[class.ngwButton]') buttonClass = true;
-}
